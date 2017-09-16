d3bdbd431bf5d05f990b1e80caff46be9e938642
Shiveen Pandita <shiveenpandita@outlook.com>
2017-09-16 20:34:50 +1000

    Added android places api key3

--- app/src/main/java/com/meet/letsmeet/ViewPreferenceResults.java
+++ app/src/main/java/com/meet/letsmeet/ViewPreferenceResults.java
@@ -1,21 +1,25 @@
 package com.meet.letsmeet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.places.GeoDataApi;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.location.places.Places;
-import com.google.android.gms.maps.GoogleMap;
 
 public class ViewPreferenceResults extends AppCompatActivity{
 
+    protected GeoDataClient geoDataClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_map_locations);
+
+        geoDataClient = Places.getGeoDataClient(this, null);
+        PlaceDetectionClient placeDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+
     }
 
 
